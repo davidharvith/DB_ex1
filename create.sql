320e13af615b2ea49dd102c163d3eee056ebac09
davidharvith <davidaviadh@gmail.com>
2025-03-31 08:53:42 -0400

    update

--- create.sql
+++ create.sql
@@ -1,3 +1,27 @@
+create table region(
+	regionname varchar primary key
+);
+
+create table country(
+	 country varchar primary key,
+	 countrycode varchar not null,
+	 region varchar not null references region(regionname)
+);
+
+create table specialized(
+	specialized BOOLEAN primary key
+);
+
+create table privatestatus(
+	private01 BOOLEAN primary key
+);
+
+
+create table phd_granting(
+	phd_granting BOOLEAN
+);
+
+
 create table enrollment(
 	country varchar, 
 	countrycode varchar, 
@@ -20,46 +44,25 @@ create table enrollment(
 
 create table University(
 	iau_id1 varchar primary key,
-	 orig_name varchar not null,
-	 yrclosed INT,
-	 foundedyr INT not null,
-	 latitude FLOAT,
-	 longitude FLOAT ,
-	 divisions INT,
-	 phd_granting BOOLEAN not null references phd_granting(phd_granting),
-	 private01 BOOLEAN not null references privatestatus(private01),
-	 country varchar not null references country(country),
-	 specialized BOOLEAN not null references specialized(specialized)
-);
-
-create table specialized(
-	specialized BOOLEAN primary key
-);
-
-create table privatestatus(
-	private01 BOOLEAN primary key
+	orig_name varchar not null,
+	yrclosed INT,
+	foundedyr INT not null,
+	latitude FLOAT,
+	longitude FLOAT ,
+	divisions INT,
+	phd_granting BOOLEAN not null references phd_granting(phd_granting),
+	private01 BOOLEAN not null references privatestatus(private01),
+	country varchar not null references country(country),
+	specialized BOOLEAN not null references specialized(specialized)
 );
 
 
-create table phd_granting(
-	phd_granting BOOLEAN
-);
 
-create table country(
-	 country varchar primary key,
-	 countrycode varchar not null,
-	 region varchar not null references region(regionname)
-	 
-);
 
-create table region(
-	regionname varchar primary key
-);
 
 create table acceptence_data(
 	year INT,
 	iau_id1 varchar references University(iau_id1),
 	students5_estimated INT,
-	primary key(year, iau_id1)
-	
+	primary key(year, iau_id1)	
 );
